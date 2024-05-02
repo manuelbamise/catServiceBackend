cc7a62bf57c0119941debf8bee972b5de66d7539
Emmanuel Oluwabamise Tobi <102730033+manuelbamise@users.noreply.github.com>
2024-05-02 14:41:18 +0100

    Built the api that fetches from the catapi server

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(private appservice: AppService) {}
+  
+  @Get('getdata')
+  async getData() {
+    return this.appservice.getData();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private config: ConfigService) {}
+  async getData(): Promise<any> {
+    try {
+      const response = await fetch(
+        `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${this.config}`,
+      );
+      return response.json();
+    } catch (err) {
+      return { msg: err.message };
+    }
   }
 }
